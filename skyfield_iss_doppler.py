fa3655eb0833b13beb8bcd99c2b4e253e8710bf7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-24 23:25:08 -0400

    functionalize with doppler example

--- skyfield_iss_doppler.py
+++ skyfield_iss_doppler.py
@@ -2,24 +2,37 @@
 """
 compute range speed of satellite from an observer.
 This allows computing Doppler shift.
+
+example
+
+    python skyfield_iss_doppler.py "ISS (ZARYA)" 40 -100
 """
-from skyfield.api import Topos, load
+from skyfield.api import Topos, load, utc
 import math
 import typing
+from datetime import datetime
+from dateutil.parser import parse
 import argparse
 
 c = 299_792_458  # m/s
 
 
-def get_range_velocity(sat_name: str) -> typing.List[float]:
-    tnow = load.timescale().now()
-    observer = Topos(42.36, -71.06)
+def get_range_velocity(sat_name: str, obs_lat: float, obs_lon: float, time: datetime) -> typing.List[float]:
+
+    ts = load.timescale()
+    if isinstance(time, str):
+        time = parse(time)
+    time = time.replace(tzinfo=utc)
+    time = ts.utc(time) if time is not None else ts.now()
+
+    observer = Topos(obs_lat, obs_lon)
+
     stations_url = "http://celestrak.com/NORAD/elements/stations.txt"
     satellites = load.tle(stations_url)
 
     sat = satellites[sat_name]
 
-    relative_position = (sat - observer).at(tnow)
+    relative_position = (sat - observer).at(time)
     # sat_velocity = sat.at(tnow).velocity.km_per_s
     range_velocity = relative_position.velocity.km_per_s
 
@@ -29,17 +42,15 @@ def get_range_velocity(sat_name: str) -> typing.List[float]:
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("satname", help="TLE name of satellie e.g. ISS (ZARYA)")
-    p.add_argument(
-        "freqMHz", help="frequency in MHz to compute Doppler shift for", type=float, nargs="?"
-    )
+    p.add_argument("obs_latlon", help="observer latitude longitude (degrees)", type=float, nargs=2)
+    p.add_argument("-t", "--time", help="UTC time of observation (default: now)", nargs="?")
+    p.add_argument("freqMHz", help="frequency in MHz to compute Doppler shift for", type=float, nargs="?")
     P = p.parse_args()
 
-    range_velocity = get_range_velocity(P.satname)
+    range_velocity = get_range_velocity(P.satname, P.obs_latlon[0], P.obs_latlon[1], P.time)
 
     # notice the range_speed matches Gpredict, etc.
-    range_speed = math.sqrt(
-        range_velocity[0] ** 2 + range_velocity[1] ** 2 + range_velocity[2] ** 2
-    )
+    range_speed = math.sqrt(range_velocity[0] ** 2 + range_velocity[1] ** 2 + range_velocity[2] ** 2)
 
     print(f"range speed [km/sec] {range_speed:.3f}")
 
